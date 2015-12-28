e4b50cbb445e84c1e07d78239e5d4651c7f12e07
KitaLeo <leo.jany@gmail.com>
2015-12-28 12:34:42 +0700

    php combo

---
+++ resources/views/front/combo.php
@@ -0,0 +1,205 @@
+<?php include 'header.php' ?>
+	<div class="contain wrap mxClrAft combo_page">
+		<div class="combo_detail left">
+			<div class="ava">
+				<img src="style/images/temp/cb1.jpg" alt="">
+			</div>
+			<div class="dtb">
+				<div class="name">
+					Combo Tony Buổi sáng (Tặng kèm ly sứ cao cấp)
+				</div>
+				<div class="sls">
+					Giá bán: 
+					<span>252.000đ</span>
+				</div>
+			</div>
+			<div class="btb">
+				<div class="cartbox mxClrAft">
+					<div class="num left">
+						<input type="text" class="n left" value="1">
+						<div class="ctrlnum left">
+							<div class="fa fa-angle-up is-up"></div>
+							<div class="fa fa-angle-down is-down"></div>
+						</div>
+						<div class="clear"></div>
+					</div>
+					<button class="addcart left">
+						<span class="fa fa-opencart"></span>
+						<span class="fa fa-check"></span>
+						THÊM VÀO GIỎ
+					</button>
+					<button class="over is-over-check">
+						<span class="fa fa-bullhorn"></span>
+						<span class="fa fa-check"></span>
+						BÁO KHI CÓ HÀNG
+					</button>
+				</div>
+			</div>
+		</div>
+		<div class="combo_item_box right">
+			<div class="book_item">
+				<div class="book_item_hover">
+					<div class="book_cover">
+						<a href="chitiet.php">
+							<img src="style/images/temp/book5.jpg" alt="mybook">
+						</a>
+					</div>
+					<div class="smdt">
+						<div class="dt">
+							<a href="chitiet.php" class="book_title">
+								Trên đường bay
+							</a>
+							<div class="book_author intt">
+								Tác giả: 
+								<a href="tacgia.php" >
+									Tony Buổi sáng
+								</a>
+							</div>
+							<div class="intt li">
+								NXB: 
+									<a href="nxb.php">Bách Việt book</a>
+							</div>
+							<div class="sal">
+								<div class="gri">
+									<div class="old">
+										103.000đ
+									</div>
+									<div class="new">
+										95.000đ
+									</div>
+								</div>
+								<div class="li">
+									<div class="old intt">
+										Giá bìa: 
+										<span>
+											103.000đ
+										</span>
+									</div>
+									<div class="new intt">
+										Giá bán: 
+											<span>
+												98.000đ
+											</span>
+									</div>
+								</div>
+							</div>
+							<div class="trig li wait">
+									Tự tin lên nào! Đừng khóc nữa ... Là những câu thần chú của tuổi trẻ khi tôi lấy hết can đảm rời xa P. Chàng trai ấy vẫn sống mãi trong tâm khảm của tôi, bởi những gì dang dở không có kết thúc, thì mãi vẫn là thứ mà người ta thường nghĩ đến, lúc không còn nơi bấu víu! Tôi mãi coi P là người tri kỷ của mình, dù P đã là thiên sứ! P đã rời khỏi thế gian này, nhưng P vẫn ở trong lòng tôi, như một ký ức bất tử. Đúng là, những gì đẹp sẽ còn sống mãi! Với tôi, P còn lớn hơn tình yêu, bởi chúng tôi đã từng có nhiều năm là bạn thân dưới một mái trường! Bước qua tan vỡ với P thật sâu lắng, day dứt và cả sự biết ơn! Bởi nếu cuộc đời này không gặp P, tôi sẽ không thể nhận ra giá trị của mình và gìn giữ nó! Để một ngày trưởng thành hơn, tôi gặp M trong tâm trạng nhớ cố hương da diết. Thứ duy nhất ở M cứ ở lì trong tim tôi không chịu rời đi, chính là ánh mắt! Ấm áp và an yên vô cùng. M, P đều vẫn còn lại trong tôi, dù nhiều thứ đã bỏ tôi đi mãi...
+							</div>
+						</div>
+						<div class="cartbox mxClrAft">
+							<div class="num left">
+								<input type="text" class="n left" value="1">
+								<div class="ctrlnum left">
+									<div class="fa fa-angle-up is-up"></div>
+									<div class="fa fa-angle-down is-down"></div>
+								</div>
+								<div class="clear"></div>
+							</div>
+							<button class="addcart left">
+								<span class="fa fa-opencart"></span>
+								<span class="fa fa-check"></span>
+								THÊM VÀO GIỎ
+							</button>
+							<button class="over is-over-check">
+								<span class="fa fa-bullhorn"></span>
+								<span class="fa fa-check"></span>
+								BÁO KHI CÓ HÀNG
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="book_item">
+				<div class="book_item_hover">
+					<div class="book_cover">
+						<a href="chitiet.php">
+							<img src="style/images/temp/book5.jpg" alt="mybook">
+						</a>
+					</div>
+					<div class="smdt">
+						<div class="dt">
+							<a href="chitiet.php" class="book_title">
+								Trên đường bay
+							</a>
+							<div class="book_author intt">
+								Tác giả: 
+								<a href="tacgia.php" >
+									Tony Buổi sáng
+								</a>
+							</div>
+							<div class="intt li">
+								NXB: 
+									<a href="nxb.php">Bách Việt book</a>
+							</div>
+							<div class="sal">
+								<div class="gri">
+									<div class="old">
+										103.000đ
+									</div>
+									<div class="new">
+										95.000đ
+									</div>
+								</div>
+								<div class="li">
+									<div class="old intt">
+										Giá bìa: 
+										<span>
+											103.000đ
+										</span>
+									</div>
+									<div class="new intt">
+										Giá bán: 
+											<span>
+												98.000đ
+											</span>
+									</div>
+								</div>
+							</div>
+							<div class="trig li wait">
+									Tự tin lên nào! Đừng khóc nữa ... Là những câu thần chú của tuổi trẻ khi tôi lấy hết can đảm rời xa P. Chàng trai ấy vẫn sống mãi trong tâm khảm của tôi, bởi những gì dang dở không có kết thúc, thì mãi vẫn là thứ mà người ta thường nghĩ đến, lúc không còn nơi bấu víu! Tôi mãi coi P là người tri kỷ của mình, dù P đã là thiên sứ! P đã rời khỏi thế gian này, nhưng P vẫn ở trong lòng tôi, như một ký ức bất tử. Đúng là, những gì đẹp sẽ còn sống mãi! Với tôi, P còn lớn hơn tình yêu, bởi chúng tôi đã từng có nhiều năm là bạn thân dưới một mái trường! Bước qua tan vỡ với P thật sâu lắng, day dứt và cả sự biết ơn! Bởi nếu cuộc đời này không gặp P, tôi sẽ không thể nhận ra giá trị của mình và gìn giữ nó! Để một ngày trưởng thành hơn, tôi gặp M trong tâm trạng nhớ cố hương da diết. Thứ duy nhất ở M cứ ở lì trong tim tôi không chịu rời đi, chính là ánh mắt! Ấm áp và an yên vô cùng. M, P đều vẫn còn lại trong tôi, dù nhiều thứ đã bỏ tôi đi mãi...
+							</div>
+						</div>
+						<div class="cartbox mxClrAft">
+							<div class="num left">
+								<input type="text" class="n left" value="1">
+								<div class="ctrlnum left">
+									<div class="fa fa-angle-up is-up"></div>
+									<div class="fa fa-angle-down is-down"></div>
+								</div>
+								<div class="clear"></div>
+							</div>
+							<button class="addcart left">
+								<span class="fa fa-opencart"></span>
+								<span class="fa fa-check"></span>
+								THÊM VÀO GIỎ
+							</button>
+							<button class="over is-over-check">
+								<span class="fa fa-bullhorn"></span>
+								<span class="fa fa-check"></span>
+								BÁO KHI CÓ HÀNG
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="clear"></div>
+			<div class="more_item">
+				<div class="cvitem">
+					<div class="a">
+						<img src="style/images/temp/more.jpg" alt="">
+					</div>
+				</div>
+				<div class="dtlitem">
+					<div class="na">
+						Ly sứ cao cấp
+					</div>
+					<div class="dt is-3r">
+						Nhà sản xuất: Gốm sứ Minh Long
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
+<?php include 'footer.php' ?>
